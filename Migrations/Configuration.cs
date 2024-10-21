6041c9b70734415d3d95cbecc5a8279cbd922fdc
912-Danciu-Diana <diana.ioana.danciu@gmail.com>
2024-10-21 17:39:00 +0300

    Initial commit for ASP.NET backend - CRUD

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace ExpensesAPI.Migrations
+{
+    using ExpensesAPI.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ExpensesAPI.Data.AppDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ExpensesAPI.Data.AppDbContext context)
+        {
+            context.Entries.Add(new Entry() { Description = "tets", IsExpense = false, Value = 10.11 });
+        }
+    }
+}
